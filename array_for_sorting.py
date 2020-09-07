b6859bfc0c6bfa4b57e5d990ca2b2f9a4a3ebe98
pwestropp <pwestropp@gmail.com>
2020-09-07 17:12:37 -0500

    Create array_for_sorting.py

---
+++ array_for_sorting.py
@@ -0,0 +1,17 @@
+#the purpose of this file is to demonstrate methods to deal with the basic problems to be solved for sorting an array
+
+temp = alist [i]
+alist[i] = alist[j]
+alist[j] = temp
+
+#pass value at point i in list alist to temp, 
+#take point i in list alist, pass value at point j in list alist
+#then write temp value, which is used to temporarly hold the value at point i, to point j in list alist
+
+a = 1
+b = 2
+
+#this format will swap values of a and b, observe
+a,b=b,a 
+
+print(a,b)
